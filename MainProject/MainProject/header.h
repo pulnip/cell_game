ace6a26058e880b913898a9ba937800e8d724be2
choiw <choiwh1008@naver.com>
2020-10-31 12:39:56 +0900

    create file about KBInput

--- MainProject/MainProject/header.h
+++ MainProject/MainProject/header.h
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <Windows.h>
 
-typedef unsigned char Bool
+typedef unsigned char Bool;
 #ifndef True
 #define True 1
 #define False 0
 #endif
 
+typedef struct _KeyState{
+    Bool bPressed;
+    Bool bToggled;
+} KeyState;
+
 int OnStart();
 int OnUpdate();
 int OnDestroy();
+void getKBInput();
