abb001444c8a9b6acfab2e7cf7e11e56c08cb098
pogery-foggy <66292699+pogery-foggy@users.noreply.github.com>
2020-11-11 22:51:02 +0900

    add _informateCpuCellScore

--- MainProject/CPU_cell/CPU_cell_notice.h
+++ MainProject/CPU_cell/CPU_cell_notice.h
@@ -1,3 +1,4 @@
+#include "base.h" // Cell 구조체를 쓰기위한 선언 하지만 폴더가 달라서 지금은 적용안됨
 /*
 세트를 받아서 DNA를 생성하는건 onstart에 있고
 세트에 따라 행동해서 맵업데이트 + 점수부여
@@ -15,7 +16,8 @@ void Get_User_Cell_information();	//유저 세포의 DNA 정보를 가져오는 함수
 void Create_DNA();	//DNA를 만드는 함수
 void CPU_Cell_Map_Update();	//CPU세포의 맵 업데이트 함수
 int Score();	//점수를 부여하는 함수
-
+// 내 생각에는 점수를 부여하는 함수형을 void로 하고 함수 내에서 컴퓨터 셀 구조체 안에있는 점수라는 변수에 접근해서 직접적으로 점수를 넣어야 할 것 같은데
+// 그러면 void score(&점수를 부여할 컴퓨터셀) 이런 식으로 되려나 아래 _informateCpuCellScore
 
 typedef struct Send_Cpucell_result {
 
@@ -23,7 +25,10 @@ typedef struct Send_Cpucell_result {
 
 void Send_Cpucell_result();	//CPU세포의 상황을 보내는 함수
 
-
+typedef sturct _informateCpuCellScore{  //이렇게 이름 길 필요는 없는데 이름만으론 못알아볼까봐 어차피 임시이름이니 길게 지었어
+	Cell informationCell;				// 이 구조체는 원래 있는 Cell 구조체에다 컴퓨터만이 가지고있는 "유전점수"를 위한 구조체
+	int score;
+}
 
 
 typedef struct Get_Set_Information {
