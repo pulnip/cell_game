2d4ef15dcb8842234f865269d5270abc3b497483
choiw <choiwh1008@naver.com>
2020-11-17 21:38:24 +0900

    debug(ing) pre-rel

--- MainProject/MainProject/infra.c
+++ MainProject/MainProject/infra.c
@@ -48,6 +48,7 @@ void getKBInput(){
     for(int i=0; i<0x100; ++i){
         KeyState lastState=keys[i];
         
+        #error "CHECK VALUE"
         short tmpKey=GetKeyState(i);
         keys[i].bPressed = (tmpKey&0x1000)>>(sizeof(short)-1);
         keys[i].bToggled = tmpKey&0x1;
