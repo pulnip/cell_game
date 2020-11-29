5733c45fe5ce669edd6aedb82daaba1ab0939de3
pogery-foggy <66292699+pogery-foggy@users.noreply.github.com>
2020-11-29 23:46:09 +0900

    name change del get

--- MainProject/MainProject/CpuMain.c
+++ MainProject/MainProject/CpuMain.c
@@ -8,15 +8,15 @@ int initComputer(void) {
 		//tempDNA createMovingSet(cell); //구조체 2차원배열 접근방법 모름 찾아봐야함 cell->set
 		//createTech(cell); //태림이가 짜놓은 코드가 뭔지 모르겠음
 		_temp.id = 2;  //Cell->id , isCell = 1
-		_temp.pos = getPos();
-		_cell.pos.x = GetRandom(0, MAP_WIDTH);  
-		_cell.pos.y = GetRandom(0, MAP_HEIGHT);
+		_temp.pos.x = GetRandom(0, MAP_WIDTH);  
+		_temp.pos.y = GetRandom(0, MAP_HEIGHT);
 		_temp.turnDNA = 0;
+		_temp.forward = _temp.pos;
 		_temp.stat = getBasicInfo();
 
 		(); // 개선이 필요합니다. cell에 대한 접근방법 필요.
 
-		Cell newCell = createCell(_temp);
+		Cell newCell = createCpuCell(_temp);
 		if (!(newCell) == NULL) {
 			count++;
 		}
@@ -38,7 +38,7 @@ int initComputer(void) {
 */
 
 int updateComputer(void) { //cell이 
-	exeCells(); // 
+	exeCpuCells(); // 
 }
 
 //Cell create (getId, get posForward
@@ -56,47 +56,3 @@ Cell* createCpuCell(Cell _temp) {
 	return cell;
 } //나중에 destroyed 필요
 
-
-isCell getId(char *_id){ //map.h isCell 구조체참조
-	if (!(strcmp(_id,"USER")) {
-		return 1;
-	}
-	else if (!(strcmp(_id, "CPU")) {
-		return 2;
-	}
-	else if (!(strcmp(_id, "NOCELL")) {
-		return 0;
-	}
-	else { return -1;} // 생성실패
-}
-
-Coord getPos(void) {
-	Cell _cell; 
-	_cell.pos.x = GetRandom(0, MAP_WIDTH);  //0이 맞나?
-	_cell.pos.y = GetRandom(0, MAP_HEIGHT);
-	return _cell.pos;
-}
-
-Coord getForward(void) {
-	Cell _cell;
-	_cell.forward.x = GetRandom(?, ? ); //? forward뭘로 할건지
-	_cell.forward.y = GetRandom(?, ? ); // "
-	return _cell.forward;
-}
-
-void _setForward(Cell* cell, Coord coord){
-	cell->forward.x=coord.x;
-	cell->forward.y=coord.y;
-}
-Coord _getForward(Cell* cell){
-	return cell->forward
-}
-
-BasicInfo getBasicInfo(void) {
-	Cell _cell;
-	_cell.healthPoint; //= 1000;   //체력
-	int attackPoint; //= 100;       //공격력
-	int defensePoint; //= 20;       //방어력
-	int starveMinusHealthPoint; //= 10; //허기?
-
-}
