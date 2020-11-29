cc826aafc67c79b142c7bc72b05603b714084e38
pogery-foggy <66292699+pogery-foggy@users.noreply.github.com>
2020-11-30 01:40:36 +0900

    will error code revise 1

--- MainProject/MainProject/CpuMain.c
+++ MainProject/MainProject/CpuMain.c
@@ -5,20 +5,24 @@ int initComputer(void) {
 	int count = 0;
 	for (int i = 0; i < PROTO_CELL_NUMBER; i++) {
 		Cell _temp;
+		BasicInfo _tempStat;
 		//tempDNA createMovingSet(cell); //구조체 2차원배열 접근방법 모름 찾아봐야함 cell->set
 		//createTech(cell); //태림이가 짜놓은 코드가 뭔지 모르겠음
 		_temp.id = 2;  //Cell->id , isCell = 1
 		_temp.pos.x = GetRandom(0, MAP_WIDTH);  
 		_temp.pos.y = GetRandom(0, MAP_HEIGHT);
 		_temp.turnDNA = 0;
-		_temp.forward = _temp.pos;
-		_temp.stat = getBasicInfo();
-		//tech 넣을자리
-
-
-		Cell newCell = createCpuCell(_temp);
-		if (!(newCell) == NULL) {
+		_temp.forward.x = _temp.pos.x;
+		_temp.forward.y = _temp.pos.y;
+		_tempStat = initBasicInfo();
+		_temp.stat.healthPoint = _tempStat.stat.healthPoint;
+		_temp.stat.defensePoint = _tempStat.stat.defensePoint;
+		_temp.stat.attackPoint = _tempStat.stat.attackPoint;
+		
+		Cell* newCell = createCpuCell(_temp);
+		if (!(newCell == NULL)) {
 			count++;
+			setCpuMovingSet(newCell);
 		}
 	}
 	if (count == PROTO_CELL_NUMBER) { return 1; } // suc
@@ -52,23 +56,24 @@ void changeIsCellPos(Cell* _cell) {  //exeCpuCells 안에 포함됨
 }
 
 
-void createCpuMovingSet(Cell* cell) {   //행동을 담당하는 세트만 만드는 함수  
+void setCpuMovingSet(Cell* cell) {   //행동을 담당하는 세트만 만드는 함수  
 	for (int i = 0; i < 8; i++) { // 구조체 cell에 접근하는 방법
 		cell->DNA[i] = BehaviourList[GetRandom(0, sizeof(BehaviourList) / 4)];
 	}
 }
 
 
-void exeCpuCells(void) { // ?
+void exeCpuCells(void) {
 	Node* n = Cells.head;
 	while (n != NULL) {
 		Cell* cell = n->pObject;
 		cell->DNA[cell->turnDNA](cell); //Do 필요없는건가
-		isCellPos(cell);
+		changeIsCellPos(cell);
+		
 		map[cell->pos.x][cell->pos.y] = True;
 		cell->turnDNA = ((cell->turnDNA) + 1) % 8; // 1~8까지의 세트 안에서만 
-
-			n = n->next;
+		n = n->next;
+		updateGame(200); //2000/200 == 10 == 0.1초?
 	}
 }
 
@@ -83,7 +88,7 @@ void Sort(Cell* set) { //미완성
 }
 
 void createNewSet(**parentSet) { //2차원배열로 왔다고 가정 //대대적인 수정필요 미완성
-	int tempSet[9];
+	int tempSet[8];
 	for (int i = 0; i < 8; i++) {
 		tempSet[i] = Behaviour[parentSet[GetRandom(0, 2)][i]]; ]
 	}
--- MainProject/MainProject/CpuMain.h
+++ MainProject/MainProject/CpuMain.h
@@ -1,12 +1,12 @@
-#include "CpuCellSet.h"
 #include "Cell_Properties.h" // 태림이 함수를 가져올 헤더파일이 없음
 #include "Cell.h"
 #include "Map.h"
+#include "Game.h"
 #include <string.h>
 #include "SuperHeader.h"
 typedef void(*DO)();
 
-void createCpuMovingSet(Cell* cell);
+void setCpuMovingSet(Cell* cell);
 void exeCpuCells(void);
 
 
